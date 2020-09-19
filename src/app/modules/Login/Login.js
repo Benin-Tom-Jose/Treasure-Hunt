d7d44809cb7b04f97b2ee29beb106548a07633da
Benin Jose <benin.jose@litmus7.com>
2020-09-19 21:53:32 +0530

    Implemented/ basic-google-auth

--- src/app/modules/Login/Login.js
+++ src/app/modules/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import './Login.scss';
 
-const Login = ()=>{
-    return(
+const Login = () => {
+    const responseGoogle = (token)=>{
+        console.log(token.tokenId);
+    };
+
+    return (
         <div className="login-page-wrapper">
             <h1>Login Page</h1>
+            <GoogleLogin
+                clientId="238080994627-h3gm4bjd7b617peas272a3t72eaomsbi.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     );
 };
