cba9e20dd4d7cff5633476a52043c69e88cb83bc
Benin Jose <benin.jose@litmus7.com>
2020-12-12 14:39:16 +0530

    Hotfix/ backend-endpoint update

--- src/config/ApiUrl.js
+++ src/config/ApiUrl.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8081/";
-export const BASE_URL = "http://15.206.15.125:8081/";
+export const BASE_URL = "http://incognito.litmus7.com:8081/";
 
 const ENDPOINTS = {
     googleLogin: "google/login",
