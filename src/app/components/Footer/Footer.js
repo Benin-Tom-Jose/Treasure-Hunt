a587dffe3e43edab5f5094a5adc5e14099966a84
Benin Jose <benin.jose@litmus7.com>
2020-12-14 15:24:49 +0530

    Hotfix/ footer fix for made-with-love design

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Icon } from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
 
 import './Footer.scss';
 
 const Footer = () => {
+    const location = useLocation();
+
     return (
         <footer className="footer-wrapper">
             <span className="copyright-container">Copyright © 2020, All Rights Reserved.</span>
-            <div className="highlight-container">
-                <span className="label">Made with <Icon className="icon">favorite</Icon></span>
-                <span className="creators">@Litmus7</span>
-            </div>
+            {
+                location && location.pathname !== "/" &&
+                <div className="highlight-container">
+                    <span className="label">Made with <Icon className="icon">favorite</Icon></span>
+                    <span className="creators">@Litmus7</span>
+                </div>
+            }
         </footer>
     );
 };
